e315b79f3e0c8a505699b82cc91aa234bef79653
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-01-22 17:34:27 +0530

    comments

---
+++ Week-3/Comments/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+namespace Task
+{
+    /// <summary>
+    /// This class has fields to store student details and 
+    /// methods to set marks and calculate average
+    /// </summary>
+    public class Student {
+        private string name;
+        private string unique_ID;
+        private int sub1, sub2, sub3;
+
+        /// <summary>
+        /// This method sets the values of each subject
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// student_object.set_marks(100,49,50);
+        /// </code>
+        /// </example>
+        /// <param name="sub1">marks in subject 1</param>
+        /// <param name="sub2">marks in subject 2</param>
+        /// <param name="sub3">marks in subject 3</param>
+        public void set_marks(int sub1,int sub2, int sub3) {
+            this.sub1 = sub1;
+            this.sub2 = sub2;
+            this.sub3 = sub3;
+        }
+
+        /// <summary>
+        /// This method calculates average of marks and return the average
+        /// </summary>
+        /// <return>
+        /// returns the average of three subject marks
+        /// </return>
+        public float calulate_averge() {
+            float avg=(this.sub1+this.sub2+this.sub3)/3f;
+            return avg;
+        }
+
+        /// <summary>
+        /// Constructor to initialize the name and unique_ID fields
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="unique_ID"></param>
+        public Student(string name, string unique_ID) {
+            this.name = name;
+            this.unique_ID = unique_ID;
+        }
+    }
+    /// <summary>
+    /// class for main program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// main class
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student("Praveen", "560236");
+            s1.set_marks(40, 20, 40);
+            Console.WriteLine(s1.calulate_averge());
+            
+        }
+    }
+}
