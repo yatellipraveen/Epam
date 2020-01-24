b139a41df0fbbd826b283cf01ab6f0f9f1062f4f
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-01-24 10:48:39 +0530

    added region and comments

---
+++ Week-3/Calculator/CalculatorLibrary/MathOperation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class MathOperation
+    {
+        #region operations
+        public MathOperation()
+        {
+        }
+        /// <summary>
+        /// performs addition operation on two integers and returns the sum
+        /// </summary>
+        /// <param name="number1">first number to add</param>
+        /// <param name="number2">second number to add</param>
+        /// <returns>
+        /// returns the sum of two numbers
+        /// </returns>
+        public int Add(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+        /// <summary>
+        /// performs subtraction of two integers
+        /// </summary>
+        /// <param name="number1">first number</param>
+        /// <param name="number2">second number</param>
+        /// <returns>returns the subtraction of two integers</returns>
+        public int Subtract(int number1, int number2)
+        {
+            int result = number1 - number2;
+            return result;
+        }
+        /// <summary>
+        /// performs division of two integers
+        /// </summary>
+        /// <param name="number1">Divident</param>
+        /// <param name="number2">Divisor</param>
+        /// <returns> returns the quotient after division</returns>
+        public double Divide(double number1, double number2)
+        {
+            if (number2 == 0) throw new DivideByZeroException("Number2 is zero , cannot divide");
+            double result = number1 / number2;
+            return result;
+        }
+        /// <summary>
+        /// peforms multiplication of two numbers and returns the result
+        /// </summary>
+        /// <param name="number1">first number</param>
+        /// <param name="number2">second number</param>
+        /// <returns>returns the result of multiplication of two numbers</returns>
+        public int Multiply(int number1, int number2)
+        {
+
+            int result = number1 * number2;
+            return result;
+        }
+        #endregion
+
+
+    }
+}
