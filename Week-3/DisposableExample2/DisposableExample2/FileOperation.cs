a7a85ca087039b59a53335cd0d124dbb30999c0e
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-01-28 19:57:39 +0530

    Disposable Example

---
+++ Week-3/DisposableExample2/DisposableExample2/FileOperation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace DisposableExample2
+{
+    public class FileOperation : IDisposable
+    {
+
+        private const string _path = @"C:\\Users\\Praveen_Yatelli\\Documents\\FileStreamExample\\Example.txt";
+        private StreamWriter writerObject;
+        /// <summary>
+        /// Write Data to File
+        /// </summary>
+        /// <param name="data">Input data from user that is to be written in file</param>
+        public void WriteToFile(String data)
+        {
+            writerObject = new StreamWriter(_path);
+            writerObject.WriteLine(data);
+            writerObject.Flush();
+        }
+
+        /// <summary>
+        /// Dispose method closes the StreamWriter
+        /// </summary>
+        public void Dispose() 
+        {
+            Console.WriteLine("Dispose method called");
+            writerObject.Close();
+            // GC.SuppressFinalize() tells GC not to call the Destructor
+            GC.SuppressFinalize(this);
+        }
+
+        // Finalize Implementation
+        ~FileOperation()
+        {
+            Console.WriteLine("Destructor called");
+            writerObject.Close();
+
+        }
+
+
+    }
+}
---
+++ Week-3/DisposableExample2/FileOperation/FileHandle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using DisposableExample2;
+
+namespace FileOperationn
+{
+    class FileHandle
+    {
+        static void Main(string[] args)
+        {
+            // Dispose Example
+            FileOperation fileObject = new FileOperation();
+            fileObject.WriteToFile("Hello World");
+            //Call the Dispose method
+            fileObject.Dispose();
+
+
+
+            //GC Collect Example
+            // creating a new object with same name so that the the previous object becomes obsolete
+           // fileObject = new FileOperation();
+            // Calls the Garbage Collector hence Destructor will be invoked if Dispose was not called before.
+            // comment dispose method. else will throw exception
+            GC.Collect();
+
+
+            // Using block Example
+            String path = "C:\\Users\\Praveen_Yatelli\\Documents\\FileStreamExample\\Example.txt";
+            using (StreamWriter writerObject = new StreamWriter(path))
+            {
+                writerObject.WriteLine("Using block example");
+                writerObject.Flush();
+            }
+            // Will throw Exception if using block is not used
+            StreamWriter writerObject2 = new StreamWriter(path);
+            writerObject2.Close();
+
+
+          
+        }
+    }
+}
